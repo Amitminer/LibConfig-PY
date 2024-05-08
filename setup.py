ebb6a284d5ca0fed415146de28362226b1a00e0c
AmitxD <81667979+Amitminer@users.noreply.github.com>
2024-05-08 13:01:59 +0530

    release v1

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='libconfig-py',
+    version='1.0.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=['pyyaml'],
+    author='amitminer',
+    author_email='amitxd4871@gmail.com',
+    description='A library for working with YAML configuration files',
+    url='https://github.com/Amitminer/LibConfig-PY',
+)
