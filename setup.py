e93c582971f270b49d6c41a83d6a2fe572a5adb4
AmitxD <81667979+Amitminer@users.noreply.github.com>
2024-09-23 22:42:44 +0530

    removed something

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     packages=find_packages(),
     install_requires=['pyyaml'],
     author='amitminer',
-    author_email='amitxd4871@gmail.com',
     description='A library for working with YAML configuration files',
     url='https://github.com/Amitminer/LibConfig-PY',
 )
